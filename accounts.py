2fa3054a86df352162e3cb91d315613e459fd279
jenny-lemon <jenny@lemonclean.com.tw>
2026-04-09 12:19:16 +0800

    Add files via upload

---
+++ accounts.py
@@ -0,0 +1,32 @@
+ACCOUNTS = {
+    "台北": {
+        "email": "jenny@lemonclean.com.tw",
+        "password": "jenny",
+        "folder": "01.台北專員",
+        "address_keywords": ["台北市", "新北市"],
+    },
+    "台中": {
+        "email": "jenny.tc@lemonclean.com.tw",
+        "password": "jenny",        
+        "folder": "02.台中專員",
+        "address_keywords": ["台中"],
+    },
+    "桃園": {
+        "email": "jenny.tyc@lemonclean.com.tw",
+        "password": "jenny",        
+        "folder": "04.桃園專員",
+        "address_keywords": ["桃園", "新北市"],
+    },
+    "新竹": {
+        "email": "jenny.hc@lemonclean.com.tw",
+        "password": "jenny",        
+        "folder": "05.新竹專員",
+        "address_keywords": ["新竹"],
+    },
+    "高雄": {
+        "email": "jenny.hc@lemonclean.com.tw",
+        "password": "jenny",       
+        "folder": "0.高雄專員",
+        "address_keywords": ["高雄", "台南"],
+    },
+}
